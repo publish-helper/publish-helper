444137afcbacf2dcc8e89c3ef0d716593563c1d6
bjdbjd <bjd2003bjd@gmail.com>
2024-02-13 23:26:50 +0800

    更新开源许可证

--- ph-bjd/main.py
+++ ph-bjd/main.py
@@ -1,4 +1,4 @@
-#    Copyright (c) 2024 BJD
+#    Copyright (c) 2023 BJD
 #    Publish Helper is licensed under Mulan PSL v2.
 #    You can use this software according to the terms and conditions of the Mulan PSL v2.
 #    You may obtain a copy of Mulan PSL v2 at:
