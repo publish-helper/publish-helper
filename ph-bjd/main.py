0499fa2fecbe49a9953ca46422c6e5a6bb393f94
布吉岛 <135454777+bjdbjd@users.noreply.github.com>
2024-04-11 00:29:06 +0800

    Feat: 新增支持象站官方图床。

--- ph-bjd/main.py
+++ ph-bjd/main.py
@@ -8,7 +8,7 @@ The licensing of this program is under the GNU General Public License version 3 
 For more information on this license, you can visit https://www.gnu.org/licenses/gpl-3.0.html
 """
 
-# 打包编译方式：安装Python 3.10或更高版本，执行pip install pyinstaller，安装所有相关模块后，再在"main.py"所在目录下执行下面的代码。
+# 打包编译方式：安装Python 3.10或更高版本，执行pip install pyinstaller，安装“requirements.txt中的”所有相关模块后，在项目根目录下执行下面的代码。
 # 打包编译代码：pyinstaller -F -w -i static/ph-bjd.ico ph-bjd/main.py -n "Publish Helper.exe"
 # 打包编译后需要将项目根目录下的"static"文件夹和"temp"文件夹拷贝到"Publish Helper.exe"所在文件夹中方可运行。
 # 项目仓库地址：https://github.com/publish-helper/publish-helper-gui
